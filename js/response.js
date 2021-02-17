fde2e56b8cf26cd00e3fefdd4fdc305ea2dff789
Raghunandan <raghunandan1541.cse18@chitkara.edu.in>
2021-02-17 22:52:32 +0530

    added scroll to top button

--- js/response.js
+++ js/response.js
@@ -1,8 +1,23 @@
-function myToggle() {
+const myToggle = (() => {
 	var x = document.getElementById("nav-item");
 	if (x.style.display === "block") {
 		x.style.display = "none";
 	} else {
 		x.style.display = "block";
 	}
+});
+
+var mybutton = document.getElementById("Top");
+window.onscroll = () => {scrollFunction()};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+		mybutton.style.display = "block";
+	} else {
+		mybutton.style.display = "none";
+	}
 }
+const topFunction = (() => {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+});
